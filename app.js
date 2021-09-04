5c1800776b0a424855b8ff46933c1b21473cac20
TrisC0 <54478295+TristanJPG@users.noreply.github.com>
2021-09-04 11:49:44 -0400

    Add files via upload

---
+++ app.js
@@ -0,0 +1,27 @@
+var button = document.querySelector('#toggler');
+
+button.onclick = function(){
+ var img = document.querySelector('img');
+ img.src = "images/singing_frogs.gif"
+}
+
+var button = document.querySelector('#toggler1');
+
+button.onclick = function(){
+ var img = document.querySelector('img');
+ img.src = "images/cute_frog.gif"
+}
+
+var button = document.querySelector('#toggler2');
+
+button.onclick = function(){
+ var img = document.querySelector('img');
+ img.src = "images/running_frog.gif"
+}
+
+var button = document.querySelector('#toggler3');
+
+button.onclick = function(){
+ var img = document.querySelector('img');
+ img.src = "images/pepe_dsnce.gif"
+}
